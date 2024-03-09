2cd7daa7270660c901e7bfb11321a283fb79001e
Oluwasegun Lawrence <lawrencesegun025@gmail.com>
2024-03-09 21:46:16 +0100

    client rendering for static paths

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const SITE_TITLE = "Tech People Blog";
+export const SITE_DESCRIPTION = "A blog for tech people";
+export const HOMEPAGE_ARTICLES_LIMIT = 4;
+export const ARTICLE_PER_PAGE = 5;
